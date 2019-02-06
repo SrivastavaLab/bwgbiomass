3cf0489a48a1b5b4623e20f83f48ca882d94d94b
Alathea <alathea@lutrisanalytics.com>
2019-02-06 13:26:06 -0800

    moved filter function to new script

--- R/data_cleanup_internal.R
+++ R/data_cleanup_internal.R
@@ -62,35 +62,6 @@ calculate_biomass <- function(allometry_data){
   return(allometry_matrix)
 }
 
-##' Filter by data type.
-##'
-##' @title Filter by data type
-##'
-##' @param allometry_data The allometry data set loaded from:
-##'   \url{https://github.com/SrivastavaLab/allometrydata/}
-##'
-##' @param measurement logical indicating whether the measurements should be filtered into
-##'   the final data set (type = field_measurement). Otherwise, the category
-##'   definitions will be filtered into the final data set
-##'   (type = category_definition). Defaults to \emph{measurement = TRUE}
-##'
-##' @return the filtered data set
-##'
-##' @keywords internal
-##' @importFrom dplyr %>%
-##' @export
-filter_type <- function(allometry_data, measurement = TRUE){
-  if(measurement){
-    filtered_data <- allometry_data %>%
-      dplyr::filter(type == "field_measurement")
-  } else {
-    filtered_data <- allometry_data %>%
-      dplyr::filter(type == "category_definition")
-  }
-
-  return(filtered_data)
-}
-
 ##' Standardize developmental stages. Possible stages are: "larva", "pupa",
 ##' "adult" or "NA".
 ##'
