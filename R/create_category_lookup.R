add6c35bf2d91e1a13a9488b564a1edf136370c9
Alathea <alathea@lutrisanalytics.com>
2019-02-06 14:03:41 -0800

    renamed file

--- R/create_category_lookup.R
+++ R/create_category_lookup.R
@@ -1,3 +1,24 @@
+##' Create the category lookup table. This matches a size category to a length
+##' estimate so that biomasses can be calculated on categorical sizes.
+##'
+##' @title Create category lookup table
+##'
+##' @param category_lookup The category lookup table.
+##'
+##' @return The category lookup table as a data frame.
+##'
+##' @keywords
+##' @importFrom dplyr %>%
+##' @export
+create_category_lookup <- function(){
+  category_lookup <- allometry() %>%
+    add_missing_bwg_names() %>%
+    filter_type(measurement = FALSE) %>%
+    select_category_cols()
+
+  return(category_lookup)
+}
+
 ##' Select the relevant columns for the category lookup table. length_mm is
 ##' renamed to length_est_mm as the lengths are estimates for category
 ##' definitions.
